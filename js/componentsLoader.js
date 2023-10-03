e5f5bf8af1286999720d95216d055998490400e8
MartaKMB <muszkamarta@interia.pl>
2023-10-03 13:31:57 +0200

    added All Books Section Component and wip at scss

--- js/componentsLoader.js
+++ js/componentsLoader.js
@@ -1,4 +1,7 @@
 $(function () {
   $('#for-header').load('/html/header.html #header-component');
   $('#for-author-section').load('/html/authorSection.html #author-component');
+  $('#for-all-books-section').load(
+    '/html/allBooksSection.html #all-books-component'
+  );
 });
