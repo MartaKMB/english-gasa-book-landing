231ae78f00993bca737bf0632f014a1993e80c98
MartaKMB <muszkamarta@interia.pl>
2023-10-11 17:00:35 +0200

    added gasa section and start work at baner

--- js/componentsLoader.js
+++ js/componentsLoader.js
@@ -6,12 +6,20 @@ const addScriptForComponent = (scriptPath) => {
 
 $(function () {
   $('#for-header').load('/html/header.html #header-component');
-  $('#for-author-section').load('/html/authorSection.html #author-component');
-  $('#for-all-books-section').load(
-    '/html/allBooksSection.html #all-books-component'
+  $('#for-about-gasa-section').load(
+    '/html/aboutGasaSection.html #about-gasa-component'
+  );
+  $('#for-book-baner-section').load(
+    '/html/bookBanerSection.html #book-baner-component'
   );
   $('#for-book-inside-section').load(
     '/html/bookInsideSection.html #book-inside-component'
+  );
+  $('#for-about-author-section').load(
+    '/html/aboutAuthorSection.html #about-author-component'
+  );
+  $('#for-all-books-section').load(
+    '/html/allBooksSection.html #all-books-component'
   );
   $('#for-review-section').load(
     '/html/reviewSection.html #review-component',
